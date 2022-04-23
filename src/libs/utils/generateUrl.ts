274a68ecba8d51564a735df5b1a67d902eab9b8e
leejs1030 <leejs1030@korea.ac.kr>
2022-04-24 03:23:13 +0900

    generateUrl 타입 정의 변경

--- src/libs/utils/generateUrl.ts
+++ src/libs/utils/generateUrl.ts
@@ -1,8 +1,10 @@
+import { probInfo } from 'custom-type';
+
 const generateProbUrl = (
   pageUuid: string,
   subPageUuid: string,
   paragraphUuid: string,
-  { contentid, trialid, probtableid }: { contentid: string, trialid: string, probtableid: string },
+  { contentid, trialid, probtableid }: probInfo,
 ) =>
   `/pages/${pageUuid}/sub-pages/${subPageUuid}/paragraphs/${paragraphUuid}/probs?contentid=${contentid}&trialid=${trialid}&probtableid=${probtableid}`;
 
