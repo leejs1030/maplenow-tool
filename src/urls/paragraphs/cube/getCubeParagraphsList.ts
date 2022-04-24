718fec51c5688f2f945c4ff56433d6c8c6d649bd
leejs1030 <leejs1030@korea.ac.kr>
2022-04-24 17:01:22 +0900

    큐브 문단 가져오기 코드 리팩토링

--- src/urls/paragraphs/cube/getCubeParagraphsList.ts
+++ src/urls/paragraphs/cube/getCubeParagraphsList.ts
@@ -4,15 +4,7 @@ import errors from '@errors';
 import axios from '@libs/axios';
 import Subpages from '../../subpages';
 
-const getCubeRankUpParagraphsList = async (
-  isMiracle: boolean,
-  date?: Date,
-): Promise<{
-  pageUuid: string,
-  subPageUuid: string,
-  paragraphs: { uuid: string, autoTable: any }[],
-}> => {
-  const selected = isMiracle ? cubePageEnum.miracleRankUp : cubePageEnum.rankUp;
+const getCubePragraphList = async (isMiracle: boolean, selected: cubePageEnum, date?: Date) => {
   const { pageUuid, subPages } = await Subpages.getCubeSubPageList(selected);
   let newDate;
   if (!date) newDate = new Date();
@@ -33,6 +25,18 @@ const getCubeRankUpParagraphsList = async (
   };
 };
 
+const getCubeRankUpParagraphsList = async (
+  isMiracle: boolean,
+  date?: Date,
+): Promise<{
+  pageUuid: string,
+  subPageUuid: string,
+  paragraphs: { uuid: string, autoTable: any }[],
+}> => {
+  const selected = isMiracle ? cubePageEnum.miracleRankUp : cubePageEnum.rankUp;
+  return getCubePragraphList(isMiracle, selected, date);
+};
+
 const getCubeOptionParagraphsList = async (
   isMiracle: boolean,
   date?: Date,
@@ -42,24 +46,7 @@ const getCubeOptionParagraphsList = async (
   paragraphs: { uuid: string, autoTable: any }[],
 }> => {
   const selected = isMiracle ? cubePageEnum.miracleOption : cubePageEnum.option;
-  const { pageUuid, subPages } = await Subpages.getCubeSubPageList(selected);
-  let newDate;
-  if (!date) newDate = new Date();
-  else newDate = new Date(date);
-  let i;
-  for (i = 0; i < subPages.length; i += 1)
-    if (utils.translateDescriptionToDate(subPages[i].description) <= newDate) break;
-  if (isMiracle) i = 0;
-  else if (i >= subPages.length) throw new errors.InvalidDateError();
-  const ret = await axios.get(`/pages/${pageUuid}?subPageUuid=${subPages[i].uuid}`);
-  if (ret.status !== 200) throw new errors.NexonNowError();
-  return {
-    pageUuid,
-    subPageUuid: subPages[i].uuid,
-    paragraphs: ret.data.data.selectedSubPage.paragraphs
-      .filter((value: any) => value.autoTable)
-      .map((value: any) => ({ uuid: value.uuid, autoTable: value.autoTable })),
-  };
+  return getCubePragraphList(isMiracle, selected, date);
 };
 
 export default {
