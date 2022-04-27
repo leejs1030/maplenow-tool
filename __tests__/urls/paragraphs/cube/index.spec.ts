894a06bde4ee5430a55234336c0dd57230e04a7e
leejs1030 <leejs1030@korea.ac.kr>
2022-04-27 22:50:30 +0900

    abil 함수 테스트 코드 강화 및 jest설정 변경

--- __tests__/urls/paragraphs/cube/index.spec.ts
+++ __tests__/urls/paragraphs/cube/index.spec.ts
@@ -1,16 +1,5 @@
 import Urls from '@urls';
-import { AutoTable } from 'custom-type';
-
-const testUuids = (res: {
-  pageUuid: string,
-  subPageUuid: string,
-  paragraphs: { uuid: string, autoTable: AutoTable }[],
-}) => {
-  expect(typeof res.pageUuid).toBe('string');
-  expect(typeof res.subPageUuid).toBe('string');
-  res.paragraphs.forEach((value) => expect(typeof value.uuid).toBe('string'));
-  expect(typeof res.paragraphs[0].uuid).toBe('string');
-};
+import { testUuids } from '../testUuid';
 
 describe('test for paragraphs ', () => {
   describe('cube ', () => {
