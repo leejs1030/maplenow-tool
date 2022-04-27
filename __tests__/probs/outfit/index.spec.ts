3d528629c4eebdde4cbc496ae1b14c8969d7f4ff
leejs1030 <leejs1030@korea.ac.kr>
2022-04-28 00:17:08 +0900

    outfit prob에 대한 테스트 코드 수정

--- __tests__/probs/outfit/index.spec.ts
+++ __tests__/probs/outfit/index.spec.ts
@@ -3,19 +3,19 @@ import { masterPieceNameEnum } from 'custom-type';
 
 const testForMasterPiece = async (pieceName: masterPieceNameEnum) => {
   const res = await Probs.Outfits.getMasterPieceProbs(pieceName);
+
   expect(Array.isArray(res.male.hat)).toBeTruthy();
   expect(Array.isArray(res.male.cloth)).toBeTruthy();
   expect(Array.isArray(res.male.capeAndGloves)).toBeTruthy();
   expect(Array.isArray(res.male.shoes)).toBeTruthy();
   expect(Array.isArray(res.male.weapon)).toBeTruthy();
-  expect(res.male.hat[0].probtable_name.substring(-4)).toBe('(남자)');
 
   expect(Array.isArray(res.female.hat)).toBeTruthy();
   expect(Array.isArray(res.female.cloth)).toBeTruthy();
   expect(Array.isArray(res.female.capeAndGloves)).toBeTruthy();
   expect(Array.isArray(res.female.shoes)).toBeTruthy();
   expect(Array.isArray(res.female.weapon)).toBeTruthy();
-  expect(res.male.hat[0].probtable_name.substring(-4)).toBe('(여자)');
+  expect(res.female.hat[0].probtable_name.slice(-4)).toBe('(여자)');
 };
 
 describe('test for outfit', () => {
