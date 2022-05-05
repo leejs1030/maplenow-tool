3f6fd3461a2b5c7d0b87326d42d4c75ee9c22292
leejs1030 <leejs1030@korea.ac.kr>
2022-05-06 01:16:00 +0900

    uuid로 확률 가져오기 일부 테스트 코드 추가

--- __tests__/probs/beauty/index.spec.ts
+++ __tests__/probs/beauty/index.spec.ts
@@ -1,4 +1,5 @@
 import Probs from '@probs';
+import Urls from '@urls';
 
 describe('test for beauty', () => {
   describe(' normal', () => {
@@ -14,6 +15,14 @@ describe('test for beauty', () => {
           expect(value.probtable_name.includes('여자')).toBeTruthy();
         });
       });
+
+      it('current uuid', async () => {
+        const uuidInfos = await Urls.Paragraphs.Beauty.getRoyalHairParagraphList(false);
+        const func = Probs.Beauty.getRoyalHairProbs;
+        const [a, b] = await Promise.all([func(), func(uuidInfos)]);
+        expect(JSON.stringify(a)).toBe(JSON.stringify(b));
+      });
+
       it('month - 1', async () => {
         const res = await Probs.Beauty.getRoyalHairProbs(2022, 1);
         expect(res.male[0].trialresult_name).toBe('메모리 헤어');
@@ -28,6 +37,7 @@ describe('test for beauty', () => {
         });
       });
     });
+
     describe('  face', () => {
       it('current', async () => {
         const res = await Probs.Beauty.getRoyalFaceProbs();
@@ -40,6 +50,14 @@ describe('test for beauty', () => {
           expect(value.probtable_name.includes('여자')).toBeTruthy();
         });
       });
+
+      it('current uuid', async () => {
+        const uuidInfos = await Urls.Paragraphs.Beauty.getRoyalFaceParagraphList(false);
+        const func = Probs.Beauty.getRoyalFaceProbs;
+        const [a, b] = await Promise.all([func(), func(uuidInfos)]);
+        expect(JSON.stringify(a)).toBe(JSON.stringify(b));
+      });
+
       it('20220403 = 2022.03.03. 10시 이후', async () => {
         const res = await Probs.Beauty.getRoyalFaceProbs(new Date('2022-04-03'));
         expect(res.male[0].trialresult_name).toBe('스위트 캔디 얼굴');
@@ -72,7 +90,15 @@ describe('test for beauty', () => {
         });
       });
     });
-    describe('  face', () => {
+
+    it('current uuid', async () => {
+      const uuidInfos = await Urls.Paragraphs.Beauty.getRoyalHairParagraphList(true);
+      const func = Probs.Beauty.getRoyalChangeHairProbs;
+      const [a, b] = await Promise.all([func(), func(uuidInfos)]);
+      expect(JSON.stringify(a)).toBe(JSON.stringify(b));
+    });
+
+    describe('face', () => {
       it('current', async () => {
         const res = await Probs.Beauty.getRoyalChangeFaceProbs();
         expect(res.male[0].trialresult_name).toBe('차차 얼굴'); // 2022 05 05 기준
@@ -85,6 +111,13 @@ describe('test for beauty', () => {
           expect(value.content_name).toBe('로얄 성형');
           expect(value.probtable_name.includes('여자')).toBeTruthy();
         });
+      });
+
+      it('current uuid', async () => {
+        const uuidInfos = await Urls.Paragraphs.Beauty.getRoyalFaceParagraphList(true);
+        const func = Probs.Beauty.getRoyalChangeFaceProbs;
+        const [a, b] = await Promise.all([func(), func(uuidInfos)]);
+        expect(JSON.stringify(a)).toBe(JSON.stringify(b));
       });
     });
   });
